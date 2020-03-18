c3c883ddaa7a868f694c9cb77f746c169a417a68
rahuls98 <rahs98@gmail.com>
2020-03-18 12:11:39 +0530

    INIT

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os, random, copy
+import librosa
+
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
+
+def convKey(key):
+    key = set(list(key))
+    key = [ord(e) for e in key]
+    key.sort()
+    return key
+
+def genInd(key, limit):
+    c = 1
+    ind = []
+    while(True):
+        temp = [i*c for i in key]
+        if(any(c>limit for c in temp)):
+            break
+    
+        ind += temp
+        c += 269
+    
+    ind.sort()
+    return ind
+
+def encDec(data, ind):
+    start = 0
+    sec = 1
+    for i in ind:
+        stop = i
+        if sec%2==0:
+            data[start:stop] = data[start:stop][::-1]   
+
+        start = stop
+        sec += 1
+
+    stop = len(data)
+    if sec%2==0:
+        data[start:stop] = data[start:stop][::-1]
+        
+    return data
+
+@app.route('/home')
+def home():
+    upload = 'static/uploads/Violin.wav'
+    modified = 'static/audio/test.wav'
+    edk = 'test'
+    return render_template('home.html', upload=upload, modified=modified, edk=edk)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/form-handler', methods=['POST'])
+def handle_data():
+    if request.method == "POST":
+        if request.files:
+            audioUpload = request.files["audioUpload"]
+            path = os.path.join('static/uploads/', audioUpload.filename)
+            audioUpload.save(path)
+            
+            x,sr = librosa.load(path)
+            y = copy.deepcopy(x)
+            limit = len(x)
+
+            key = convKey(request.form["EDK"])
+            ind = genInd(key, limit)
+            y = encDec(y, ind)
+            
+            outPath = os.path.join('static/audio/', 'test.wav')
+            librosa.output.write_wav(outPath, y, sr)
+
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
